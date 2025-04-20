a73cd53b624a896a11048d66601838aaedd04442
Mac <doremiheev@vk.com>
2025-04-20 20:46:17 +0300

    changed login endpoint

--- backend/server/controllers/api_v1_Auth.cc
+++ backend/server/controllers/api_v1_Auth.cc
@@ -1,4 +1,5 @@
 #include "api_v1_Auth.h"
+#include "trantor/utils/Logger.h"
 #include <drogon/HttpRequest.h>
 #include <drogon/HttpResponse.h>
 #include <drogon/HttpTypes.h>
@@ -7,16 +8,26 @@
 using namespace api::v1;
 
 // Add definition of your processing function here
-void Auth::loginAsCandidate(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
-{
+void Auth::login(const HttpRequestPtr &req,
+                 std::function<void(const HttpResponsePtr &)> &&callback) {
+
+    auto status = req->getParameter("status");
+    if (status.empty()) {
+        Json::Value json;
+        json["error"] = "Missing 'status' parameter";
+        auto resp = HttpResponse::newHttpJsonResponse(json);
+        resp->setStatusCode(k400BadRequest);
+        callback(resp);
+        return;
+
+    }
+
     auto json = req->getJsonObject();
 
-    if (!json || !json->isMember("email") || !json->isMember("password")){
-
+    if (!json || !json->isMember("email") || !json->isMember("password")) {
         auto resp = HttpResponse::newHttpJsonResponse(
-            Json::Value{{"error", "Invalid req format"}}
-        );
-        resp->setStatusCode(drogon::k400BadRequest);
+            Json::Value{{"error", "Invalid request format"}});
+        resp->setStatusCode(k400BadRequest);
         callback(resp);
         return;
     }
@@ -24,104 +35,61 @@ void Auth::loginAsCandidate(const HttpRequestPtr& req, std::function<void (const
     std::string email = (*json)["email"].asString();
     std::string password = (*json)["password"].asString();
 
-    auto client = app().getDbClient();
-
-    client->execSqlAsync(
-        "SELECT id, password FROM candidates WHERE email = ?",
-        [=,callback = std::move(callback)](const drogon::orm::Result& result) {
-            if (result.empty()){
-                auto resp = HttpResponse::newHttpJsonResponse(
-                    Json::Value{{"error", "User not found"}}
-                );
-                resp->setStatusCode(drogon::k401Unauthorized);
-                callback(resp);
-                return;
-            }
-
-            auto row = result[0];
-
-            std::string id = row["id"].as<std::string>();
-            std::string pwdFromDB = row["password"].as<std::string>();
-
-            if (password == pwdFromDB){
-                req->session()->insert("authenticated", true);
-                req->session()->insert("user_id", id);
-
-                Json::Value resp;
-                resp["status"] = "succes";
-                resp["user_id"] = id;
-                callback(HttpResponse::newHttpJsonResponse(resp));
-            }
-            else{
-                auto resp = HttpResponse::newHttpJsonResponse(
-                    Json::Value{{"error", "Wrong password"}}
-                );
-                resp->setStatusCode(drogon::k401Unauthorized);
-                callback(resp);
-            }
-        },
-        [](const orm::DrogonDbException &e){
-            std::cerr << "error: "<<e.base().what() << std::endl;
-        },
-        email);
-}
-
-
-void Auth::loginAsEmpl(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
-{
-    auto json = req->getJsonObject();
-
-    if (!json || !json->isMember("email") || !json->isMember("password")){
-
+    std::string table;
+    if (status == "candidate") {
+        table = "candidates";
+    } else if (status == "empl") {
+        table = "employers";
+    } else {
         auto resp = HttpResponse::newHttpJsonResponse(
-            Json::Value{{"error", "Invalid req format"}}
-        );
-        resp->setStatusCode(drogon::k400BadRequest);
+            Json::Value{{"error", "Invalid status parameter"}});
+        resp->setStatusCode(k400BadRequest);
         callback(resp);
         return;
     }
 
-    std::string email = (*json)["email"].asString();
-    std::string password = (*json)["password"].asString();
-
+    std::string sqlString = "SELECT id, password FROM " + table + " WHERE email = ?";
+    LOG_DEBUG << "Executing SQL query: " << sqlString << "with email: " << email;
     auto client = app().getDbClient();
-
     client->execSqlAsync(
-        "SELECT id, password FROM employers WHERE email = ?",
-        [=,callback = std::move(callback)](const drogon::orm::Result& result) {
-            if (result.empty()){
+        sqlString,
+        [=, callback = std::move(callback)](const orm::Result &result) {
+            if (result.empty()) {
                 auto resp = HttpResponse::newHttpJsonResponse(
-                    Json::Value{{"error", "User not found"}}
-                );
-                resp->setStatusCode(drogon::k401Unauthorized);
+                    Json::Value{{"error", "User not found"}});
+                resp->setStatusCode(k401Unauthorized);
                 callback(resp);
                 return;
             }
 
             auto row = result[0];
+            std::string dbPassword = row["password"].as<std::string>();
 
-            std::string id = row["id"].as<std::string>();
-            std::string pwdFromDB = row["password"].as<std::string>();
-
-            if (password == pwdFromDB){
-                req->session()->insert("authenticated", true);
-                req->session()->insert("user_id", id);
-
-                Json::Value resp;
-                resp["status"] = "succes";
-                resp["user_id"] = id;
-                callback(HttpResponse::newHttpJsonResponse(resp));
-            }
-            else{
+            if (password != dbPassword) {
                 auto resp = HttpResponse::newHttpJsonResponse(
-                    Json::Value{{"error", "Wrong password"}}
-                );
-                resp->setStatusCode(drogon::k401Unauthorized);
+                    Json::Value{{"error", "Wrong password"}});
+                resp->setStatusCode(k401Unauthorized);
                 callback(resp);
+                return;
             }
+
+            // Успешная аутентификация
+            req->session()->insert("authenticated", true);
+            req->session()->insert("user_id", row["id"].as<std::string>());
+
+            Json::Value json;
+            json["status"] = "success";
+            json["user_id"] = row["id"].as<std::string>();
+
+            auto resp = HttpResponse::newHttpJsonResponse(json);
+
+            callback(resp);
         },
-        [](const orm::DrogonDbException &e){
-            std::cerr << "error: "<<e.base().what() << std::endl;
+        [callback](const orm::DrogonDbException &e) {
+            auto resp = HttpResponse::newHttpJsonResponse(
+                Json::Value{{"error", "Database error"}});
+            resp->setStatusCode(k500InternalServerError);
+            callback(resp);
         },
         email);
 }
--- backend/server/controllers/api_v1_Auth.h
+++ backend/server/controllers/api_v1_Auth.h
@@ -12,16 +12,14 @@ namespace v1
 {
 class Auth : public drogon::HttpController<Auth>
 {
-    void loginAsCandidate(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
-    void loginAsEmpl(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
+    void login(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback);
   public:
     METHOD_LIST_BEGIN
     // use METHOD_ADD to add your custom processing function here;
     // METHOD_ADD(Auth::get, "/{2}/{1}", Get); // path is /api/v1/Auth/{arg2}/{arg1}
     // METHOD_ADD(Auth::your_method_name, "/{1}/{2}/list", Get); // path is /api/v1/Auth/{arg1}/{arg2}/list
     // ADD_METHOD_TO(Auth::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
-    METHOD_ADD(Auth::loginAsCandidate, "/candidate", drogon::Post);
-    METHOD_ADD(Auth::loginAsEmpl, "/empl", drogon::Post);
+    METHOD_ADD(Auth::login, "/login", drogon::Post);
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
